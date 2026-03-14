0ab9008684ef041ffb3f68b284a12be3966894ae
ibrokhimovnba <ibrokhimovnba@gmail.com>
2026-03-14 18:10:12 +0500

    Admin Panel with ViteJS

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 import "../styles/Navbar.css";
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='navbar__container'>
-        <h1>navbar</h1>
+        <ul className='navbar__links'>
+            <li> <NavLink className={'navbar__link'} to="/products"> Products </NavLink> </li>
+            <li> <NavLink className={'navbar__link'} to="/users"> Users </NavLink> </li>
+            <li> <NavLink className={'navbar__link'} to="/cars"> Cars </NavLink> </li>
+        </ul>
     </div>
   )
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,11 +5,10 @@ import { NavLink } from 'react-router-dom';
 const Sidebar = () => {
   return (
     <div className='sidebar__container'>
-        <h1>sidebar</h1>
-        <ul>
-            <li> <NavLink to="/products"> Products </NavLink> </li>
-            <li> <NavLink to="/users"> Users </NavLink> </li>
-            <li> <NavLink to="/cars"> Cars </NavLink> </li>
+        <ul className='sidebar__links'>
+            <li> <NavLink className={'sidebar__link'} to="/products"> Products </NavLink> </li>
+            <li> <NavLink className={'sidebar__link'} to="/users"> Users </NavLink> </li>
+            <li> <NavLink className={'sidebar__link'} to="/cars"> Cars </NavLink> </li>
         </ul>
     </div>
   )
